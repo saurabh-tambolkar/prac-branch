f6880a692e2b7a4676f2a9367c755b8f6959ae60
saurabh-tambolkar <saurabhtambolkar22@gmail.com>
2024-12-06 20:44:42 +0530

    home added

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/secret',(req,res)=>{
     res.status(200).json({message:"this is secret  page"})
 })
 
+app.get('/home',(req,res)=>{
+    res.status(200).json({message:"this is home  page"})
+})
+
 app.listen('5000',()=>{
     console.log('Server is running on port 5000')
 })
